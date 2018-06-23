dbd30aacce9b245b09d7a512fb41bcb90192edd1
Soubhi Hadri <s.hadri@ou.edu>
2018-06-23 12:07:30 -0500

    added paired_devices list

--- app/src/main/java/com/hexabitz/hexabitzdemoapp/PairedDevicesActivity.java
+++ app/src/main/java/com/hexabitz/hexabitzdemoapp/PairedDevicesActivity.java
@@ -1,7 +1,15 @@
 package com.hexabitz.hexabitzdemoapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class PairedDevicesActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class PairedDevicesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paired_devices);
+
+        ListView listview = (ListView) findViewById(R.id.paired_devices_list);
+
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        List<String> s = new ArrayList<String>();
+        for(BluetoothDevice bt : pairedDevices)
+            s.add(bt.getName());
+        
+        listview.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, s));
+
     }
 }
