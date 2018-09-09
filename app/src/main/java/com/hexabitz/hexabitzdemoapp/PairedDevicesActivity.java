4df2fd1450ebd5b9100bc1012666ffc7b6db0b7a
Soubhi Hadri <s.hadri@ou.edu>
2018-09-09 13:51:38 -0500

    show available bluetooth devices and pair inside the app

--- app/src/main/java/com/hexabitz/hexabitzdemoapp/PairedDevicesActivity.java
+++ app/src/main/java/com/hexabitz/hexabitzdemoapp/PairedDevicesActivity.java
@@ -1,13 +1,17 @@
 package com.hexabitz.hexabitzdemoapp;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -27,10 +31,12 @@ public class PairedDevicesActivity extends AppCompatActivity {
     ArrayList<String> devicesNameList;
     ArrayList<String> devicesAddressList;
     Set<BluetoothDevice> pairedDevices;
+    ArrayList<BluetoothDevice> availableDevices;
     BluetoothDevice bdDevice;
     BluetoothClass bdClass;
     BluetoothAdapter mBluetoothAdapter = null;
     ListView listview;
+    CustomeArrayAdapter adapter;
 
 
 
@@ -41,66 +47,103 @@ public class PairedDevicesActivity extends AppCompatActivity {
 
         listview = (ListView) findViewById(R.id.paired_devices_list);
 
-
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        enableBluetooth();
-        mBluetoothAdapter.startDiscovery();
-        pairedDevices = mBluetoothAdapter.getBondedDevices();
-
         devicesNameList = new ArrayList<String>();
         devicesAddressList = new ArrayList<String>();
+        availableDevices = new ArrayList<BluetoothDevice>();
 
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Quick permission check
+        int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+        permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+        if (permissionCheck != 0) {
+
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+        }
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        devicesNameList.clear();
         devicesAddressList.clear();
-        for(BluetoothDevice bt : pairedDevices) {
-            devicesNameList.add(bt.getName());
-            devicesAddressList.add(bt.getAddress());
-        }
-
-
-
-        final CustomeArrayAdapter adapter = new CustomeArrayAdapter(this,
-                -1, devicesNameList);
-        listview.setAdapter(adapter);
-
-        listview.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-                Intent data = new Intent(PairedDevicesActivity.this,ModulesActivity.class);
-                data.putExtra("address",Uri.parse(devicesAddressList.get(position)).toString());
-                startActivity(data);
-            }
-        });
+        devicesNameList.clear();
+        availableDevices.clear();
+        bluetoothScanning();
     }
 
-    private void enableBluetooth() {
+    void bluetoothScanning(){
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        getApplicationContext().registerReceiver(mReceiver, filter);
+        final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(!mBluetoothAdapter.isEnabled())
         {
             mBluetoothAdapter.enable();
             Log.e("Log", "Bluetooth is Enabled");
         }
+        mBluetoothAdapter.startDiscovery();
+
+        adapter = new CustomeArrayAdapter(this,
+                -1, devicesNameList);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent data = new Intent(PairedDevicesActivity.this,ModulesActivity.class);
+//                data.putExtra("address", Uri.parse(devicesAddressList.get(position)).toString());
+//                startActivity(data);
+                bdDevice = availableDevices.get(position);
+                Boolean isBonded = false;
+                try {
+                    isBonded = bdDevice.createBond();
+                    if(isBonded)
+                    {
+                        Intent data = new Intent(PairedDevicesActivity.this,ModulesActivity.class);
+                        data.putExtra("address", Uri.parse(devicesAddressList.get(position)).toString());
+                        startActivity(data);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }//connect(bdDevice);
+                Log.e("Log", "The bond is created: "+isBonded);
+            }
+        });
+
     }
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
 
-                Log.e("Device Name: ", "device " + deviceName);
-                Log.e("deviceHardwareAddress ", "hard" + deviceHardwareAddress);
+            String action = intent.getAction();
+
+            // When discovery finds a new device
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                availableDevices.add(device);
+                Log.e("Device Name: ", "device " + device.getName());
+                Log.e("deviceHardwareAddress ", "hard" + device.getAddress());
+                devicesNameList.add(device.getName() + '\n' + device.getAddress());
+                devicesAddressList.add(device.getAddress());
+                adapter.notifyDataSetChanged();
+
             }
+//            if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+//                Intent data = new Intent(PairedDevicesActivity.this,ModulesActivity.class);
+//                data.putExtra("address", Uri.parse(device.getAddress()).toString());
+//                startActivity(data);
+//            }
+
+
         }
     };
 
@@ -108,15 +151,13 @@ public class PairedDevicesActivity extends AppCompatActivity {
 
     private class CustomeArrayAdapter extends ArrayAdapter<String> {
         private final Context context;
-        ArrayList<String> values = new ArrayList<String>();
+        List<String> values = new ArrayList<String>();
 
         public CustomeArrayAdapter(Context context, int textViewResourceId,
                                   List<String> objects) {
             super(context, textViewResourceId, objects);
             this.context = context;
-            for (int i = 0; i < objects.size(); ++i) {
-                values.add(objects.get(i));
-            }
+            values = objects;
         }
 
         @Override
@@ -128,6 +169,11 @@ public class PairedDevicesActivity extends AppCompatActivity {
             textView.setText(values.get(position));
 
             return rowView;
+        }
+
+        @Override
+        public int getCount() {
+            return values.size();
         }
 
         @Override
