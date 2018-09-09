e720216e3f647c22ec7813ae1b0c4a45cc1c1a34
Soubhi Hadri <s.hadri@ou.edu>
2018-09-09 11:35:25 -0500

    1.	check & enable bluetooth if it's off. 2.	refresh the paired devices list.

--- app/src/main/java/com/hexabitz/hexabitzdemoapp/PairedDevicesActivity.java
+++ app/src/main/java/com/hexabitz/hexabitzdemoapp/PairedDevicesActivity.java
@@ -1,12 +1,15 @@
 package com.hexabitz.hexabitzdemoapp;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,24 +26,45 @@ public class PairedDevicesActivity extends AppCompatActivity {
 
     ArrayList<String> devicesNameList;
     ArrayList<String> devicesAddressList;
+    Set<BluetoothDevice> pairedDevices;
+    BluetoothDevice bdDevice;
+    BluetoothClass bdClass;
+    BluetoothAdapter mBluetoothAdapter = null;
+    ListView listview;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paired_devices);
 
-        ListView listview = (ListView) findViewById(R.id.paired_devices_list);
+        listview = (ListView) findViewById(R.id.paired_devices_list);
 
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        enableBluetooth();
+        mBluetoothAdapter.startDiscovery();
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
 
         devicesNameList = new ArrayList<String>();
         devicesAddressList = new ArrayList<String>();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        devicesNameList.clear();
+        devicesAddressList.clear();
         for(BluetoothDevice bt : pairedDevices) {
             devicesNameList.add(bt.getName());
             devicesAddressList.add(bt.getAddress());
         }
+
+
 
         final CustomeArrayAdapter adapter = new CustomeArrayAdapter(this,
                 -1, devicesNameList);
@@ -54,8 +78,31 @@ public class PairedDevicesActivity extends AppCompatActivity {
                 startActivity(data);
             }
         });
-
     }
+
+    private void enableBluetooth() {
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            mBluetoothAdapter.enable();
+            Log.e("Log", "Bluetooth is Enabled");
+        }
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+                Log.e("Device Name: ", "device " + deviceName);
+                Log.e("deviceHardwareAddress ", "hard" + deviceHardwareAddress);
+            }
+        }
+    };
 
 
 
