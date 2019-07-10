60e9f27fb00ced37b41afd83447965903b95199b
Hani Mounla <hani.mounla@outlook.com>
2019-07-10 23:43:20 +0300

    Updated Splash screen and launcher icon

--- app/src/main/java/com/hexabitz/hexabitzdemoapp/SplashActivity.java
+++ app/src/main/java/com/hexabitz/hexabitzdemoapp/SplashActivity.java
@@ -1,16 +1,31 @@
 package com.hexabitz.hexabitzdemoapp;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, PairedDevicesActivity.class);
-        startActivity(intent);
+  /** Duration of wait **/
+  private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    setContentView(R.layout.activity_splash);
+
+    /* New Handler to start the Menu-Activity
+     * and close this Splash-Screen after some seconds.*/
+    new Handler().postDelayed(new Runnable(){
+      @Override
+      public void run() {
+        /* Create an Intent that will start the Menu-Activity. */
+        Intent mainIntent = new Intent(SplashActivity.this,PairedDevicesActivity.class);
+        startActivity(mainIntent);
         finish();
-    }
+      }
+    }, SPLASH_DISPLAY_LENGTH);
+  }
 }
